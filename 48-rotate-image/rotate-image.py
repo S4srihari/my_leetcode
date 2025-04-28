6b85d039ebaabdacc3a2b4f5ba7dbaea33d7d0bc
S4srihari <85178734+S4srihari@users.noreply.github.com>
2025-04-28 15:38:13 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.16%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,6 +4,8 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
+        
+        #90^
         #Transpose
         for i in range(n):
             for j in range(i):
@@ -11,3 +13,20 @@ class Solution:
         #mirror
         for i in range(n):
             matrix[i] = matrix[i][::-1]
+        
+        """# 180^
+        # cols mirror
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
+        # rows mirror
+        for i in range(n//2):
+            matrix[i],matrix[n-i-1] = matrix[n-i-1],matrix[i]"""
+
+        """#270^ or -90^
+        # transpose
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        #mirror rows
+        for i in range(n//2):
+            matrix[i],matrix[n-i-1] = matrix[n-i-1], matrix[i]"""
