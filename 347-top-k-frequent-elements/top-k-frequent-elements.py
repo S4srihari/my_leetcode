e7c9d155c0fe54b694c5dfd25363b92e2f5a4957
S4srihari <85178734+S4srihari@users.noreply.github.com>
2025-05-08 14:44:20 -0400

    Time: 0 ms (100.00%) | Memory: 21.2 MB (80.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dic = {}
+        """dic = {}
         for num in nums:
             dic[num] = dic.get(num, 0) + 1
         l = [i for i in dic.items()]
@@ -8,4 +8,9 @@ class Solution:
         topK = []
         for i in range(k):
             topK.append(l[i][0])
-        return topK
+        return topK"""
+
+        c = Counter(nums)
+        top = c.most_common()
+        topk = [k for k,v in top[:k]]
+        return topk
