37d5d28b4840342482ee810b94a53ae658f245e0
S4srihari <85178734+S4srihari@users.noreply.github.com>
2025-05-08 14:20:31 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.66%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        n = len(nums)
+
+        def recurse(first, curr, nums):
+            output.append(curr[:])
+            for i in range(first,n):
+                curr.append(nums[i])
+                recurse(i+1, curr, nums)
+                curr.pop()
+        
+        recurse(0,[],nums)
+        return output
