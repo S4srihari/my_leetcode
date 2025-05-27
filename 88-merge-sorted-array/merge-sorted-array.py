3a8a21bde3fa034d0c93cb1bb2143c682d3e16b1
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-05-27 17:42:46 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (71.53%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,7 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i,j = 0,0
+
+        """i,j = 0,0
         while i < m+n and j < n:
             if nums1[i] > nums2[j]:
                 nums1.insert(i,nums2[j])
@@ -11,4 +12,17 @@ class Solution:
                 j += 1
             i += 1
         if n > j:
-            nums1[-(n-j):] = nums2[j:]
+            nums1[-(n-j):] = nums2[j:]"""
+
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
