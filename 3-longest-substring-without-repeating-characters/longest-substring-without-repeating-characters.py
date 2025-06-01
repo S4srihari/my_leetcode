3d09f81e41aae22b609821e248152cfde5da054f
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-01 11:38:58 -0400

    Time: 16 ms (68.10%) | Memory: 18 MB (12.29%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,11 +4,13 @@ class Solution:
         dic = set()
         substr, long_substr = 0, 0
         for right in range(len(s)):
-            if s[right] in dic:
-                while s[right] in dic:
-                    dic.remove(s[left])
-                    left += 1
+            if s[right] not in dic:
+                dic.add(s[right])
+                substr = right - left + 1
+                long_substr = max(substr, long_substr)
+                continue
+            while s[right] in dic:
+                dic.remove(s[left])
+                left += 1
             dic.add(s[right])
-            substr = right - left + 1
-            long_substr = max(substr, long_substr)
         return long_substr
