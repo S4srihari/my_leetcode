870434b0181b973cdeabec4942cff6ce4a504933
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-04 17:22:49 -0400

    Time: 37 ms (81.82%) | Memory: 21.3 MB (22.86%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,16 +1,46 @@
-class MyHashMap:
+class ListNode:
+    def __init__(self, key, val, nxt):
+        self.key = key
+        self.val = val
+        self.next = nxt
 
+class MyHashMap:
     def __init__(self):
-        self.arr = [-1]*1000001
+        self.size = 10007
+        self.mult = 12582917
+        self.map = [None for _ in range(self.size)]
+    
+    def hash(self,key):
+        return (key*self.mult)%self.size
 
     def put(self, key: int, value: int) -> None:
-        self.arr[key] = value
+        self.remove(key)
+        h = self.hash(key)
+        Node = ListNode(key, value, self.map[h])
+        self.map[h] = Node
 
     def get(self, key: int) -> int:
-        return self.arr[key]
+        h = self.hash(key)
+        Node = self.map[h]
+        while Node:
+            if Node.key == key:
+                return Node.val
+            Node = Node.next
+        return -1
 
     def remove(self, key: int) -> None:
-        self.arr[key] = -1
+        h = self.hash(key)
+        Node = self.map[h]
+        if not Node :
+            return
+        if Node.key == key:
+            self.map[h] = Node.next
+            return
+        while Node.next:
+            if Node.next.key == key:
+                Node.next = Node.next.next
+                return
+            Node = Node.next
 
 
 # Your MyHashMap object will be instantiated and called as such:
