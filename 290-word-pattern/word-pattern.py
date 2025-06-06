de763e5c9595dab74a7dabb701cd3d9f150d3cbb
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-05 22:25:37 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (15.08%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,6 +1,23 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        """l = s.split(" ")
+        if len(pattern) != len(l):
+            return False
+        return len(set(pattern)) == len(set(l)) == len(set(zip(pattern,l)))"""
+
         l = s.split(" ")
         if len(pattern) != len(l):
             return False
-        return len(set(pattern)) == len(set(l)) == len(set(zip(pattern,l)))
+        
+        l_hash = defaultdict(int)
+        p_hash = defaultdict(int)
+        for idx in range(len(pattern)):
+            if pattern[idx] not in p_hash:
+                p_hash[pattern[idx]] = idx
+            if l[idx] not in l_hash:
+                l_hash[l[idx]] = idx
+            
+            if p_hash[pattern[idx]] != l_hash[l[idx]]:
+                return False
+            
+        return True
