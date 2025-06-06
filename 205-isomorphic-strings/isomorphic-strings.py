3fca9edb5dc9175702ae6b7d9402ffd1494d3994
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-05 21:01:24 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (55.23%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_hash = defaultdict(int)
+        t_hash = defaultdict(int)
+        for idx in range(len(s)):
+            if s[idx] not in s_hash:
+                s_hash[s[idx]] = idx
+            
+            if t[idx] not in t_hash:
+                t_hash[t[idx]] = idx
+            
+            if s_hash[s[idx]] != t_hash[t[idx]]:
+                return False
+        return True
