d9bee9ef446179952cb6f8eaec6a70d5bc9cad02
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-08 12:51:45 -0400

    Time: 29 ms (71.40%) | Memory: 20.4 MB (71.55%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -2,19 +2,18 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
         ans = 0
-        pre_sum = []
-        sub = 0
+        temp = 0
+        dic = {}
+        dic[0] = 1
         for i in range(n):
-            sub += nums[i]
-            pre_sum.append(sub)
-        p_hash = {}
-        p_hash[0] = 1
-        for i in range(n):
-            if pre_sum[i] - k in p_hash:
-                ans += p_hash[pre_sum[i] - k]
+            temp += nums[i]
 
-            if pre_sum[i] not in p_hash:
-                p_hash[pre_sum[i]] = 1
+            if temp - k in dic:
+                ans += dic[temp - k]
+
+            if temp not in dic:
+                dic[temp] = 1
             else :
-                p_hash[pre_sum[i]] += 1
+                dic[temp] += 1
+
         return ans
