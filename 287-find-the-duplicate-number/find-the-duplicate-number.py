7f6f78448be83de55726adcbeb2b737ea5aa2b57
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-09 22:43:14 -0400

    Time: 34 ms (35.04%) | Memory: 29.1 MB (90.51%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -4,10 +4,23 @@ class Solution:
         for idx in range(1,len(nums)):
             if nums[idx] == nums[idx-1]:
                 return nums[idx]"""
-        n = len(nums)
+
+        """n = len(nums)
         k = [0]*n
         for num in nums:
             if k[num] == 1:
                 return num
-            k[num] = 1
-                
+            k[num] = 1"""
+        
+        # Floyd's Tortoise and Hare Algorithm
+        slow,fast = nums[0], nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
