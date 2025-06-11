4d3164d43e533747d5ae705477caabcc060a8c96
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-11 01:39:06 -0400

    Time: 8 ms (61.61%) | Memory: 20.3 MB (27.97%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.py
@@ -0,0 +1,17 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if not nums:
+            return
+        c = Counter(nums)
+        l = sorted([(k, k*v) for k,v in c.items()], key = lambda x : x[0])
+        if len(l) < 2:
+            return l[0][1]
+        prev, prev2 = l[0][1],0
+        for idx in range(1,len(l)):
+            if l[idx][0] - l[idx-1][0] > 1:
+                cur = max(prev,prev2) + l[idx][1]
+            else :
+                cur = max(prev, prev2 + l[idx][1])
+            prev2 = prev
+            prev = cur
+        return prev
