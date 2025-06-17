ced5ed566c7d21e8276cd4d44d43fb452f61fd3b
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-17 15:12:19 -0400

    Time: 2177 ms (15.40%) | Memory: 33 MB (44.17%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,25 +1,21 @@
 class Solution:
+    def subset_sum(self,idx, target, arr,dp):
+        if target == 0: return True
+        elif idx == 0: return arr[0] == target
+        elif dp[idx][target] != -1 :
+            return dp[idx][target]
+        else :
+            not_take = self.subset_sum(idx-1,target,arr,dp)
+            take = False
+            if target-arr[idx] >= 0:
+                take = self.subset_sum(idx-1,target-arr[idx],arr,dp)
+            dp[idx][target] = take or not_take 
+        return dp[idx][target]
+        
     def canPartition(self, nums: List[int]) -> bool:
-        s = sum(nums)
-        if s%2 == 1: return False
-        half = s//2
-        dp = set()
-        dp.add(0)
-        for num in nums:
-            temp = set()
-            for p in dp:
-                if num + p == half:
-                    return True
-                temp.add(num+p)
-                temp.add(p)
-            dp = temp
-        return False
-
-
-        """dp = [True] + [False]*half
-        for num in nums:
-            for i in range(half, num-1,-1):
-                dp[i] = dp[i] or dp[i-num]
-            if dp[-1] :return True
-
-        return False """
+        total_sum = sum(nums)
+        if total_sum%2 == 1:
+            return False
+        else:
+            dp = [[-1]*((total_sum//2)+1) for _ in range(len(nums))]
+            return self.subset_sum(len(nums)-1,total_sum//2, nums,dp)
