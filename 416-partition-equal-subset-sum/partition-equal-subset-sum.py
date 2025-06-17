ff53360a9f0ab2302fa9aa59bdd9bb82f8672299
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-17 15:48:19 -0400

    Time: 1671 ms (25.41%) | Memory: 33.3 MB (38.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,21 +1,23 @@
 class Solution:
-    def subset_sum(self,idx, target, arr,dp):
-        if target == 0: return True
-        elif idx == 0: return arr[0] == target
-        elif dp[idx][target] != -1 :
-            return dp[idx][target]
-        else :
-            not_take = self.subset_sum(idx-1,target,arr,dp)
-            take = False
-            if target-arr[idx] >= 0:
-                take = self.subset_sum(idx-1,target-arr[idx],arr,dp)
-            dp[idx][target] = take or not_take 
-        return dp[idx][target]
-        
+    def subset_sum(self,target, arr):
+        dp = [[0]*(target+1) for _ in range(len(arr))]
+        for i in range(len(arr)):
+            dp[i][0] = True
+        if arr[0] > target: return False
+        dp[0][arr[0]] = True 
+        for idx in range(1,len(arr)):
+            for t in range(target+1):
+                not_take = dp[idx-1][t]
+                take = False
+                if t >= arr[idx]:
+                    take = dp[idx-1][t-arr[idx]]
+                dp[idx][t] = take or not_take 
+        return dp[len(arr)-1][target] is True
+
     def canPartition(self, nums: List[int]) -> bool:
         total_sum = sum(nums)
         if total_sum%2 == 1:
             return False
         else:
-            dp = [[-1]*((total_sum//2)+1) for _ in range(len(nums))]
-            return self.subset_sum(len(nums)-1,total_sum//2, nums,dp)
+            return self.subset_sum(total_sum//2,nums)
+            
