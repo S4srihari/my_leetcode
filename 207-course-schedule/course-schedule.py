89ab0276124baef71cba2d57ef0b4743f4abfcfa
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-22 18:10:37 -0400

    Time: 7 ms (46.96%) | Memory: 19.5 MB (30.90%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,59 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        """adj_lis = defaultdict(list)
+        for pre in prerequisites :
+            adj_lis[pre[0]].append(pre[1])
+        course_com = [False]*numCourses
+        
+        def dfs(course,lis):
+            if not adj_lis[course]:
+                course_com[course] = True
+                return True
+            elif course_com[course]:
+                return True
+            temp = []
+            for req in adj_lis[course]:
+                if req in lis:
+                    return False
+                else:
+                    lis.append(course)
+                    temp.append(dfs(req,lis))
+                    lis.pop()
+            if all(temp):
+                course_com[course] = True
+                return True
+
+        for i in range(numCourses):
+            if not adj_lis[i]:
+                course_com[i] = True
+            if not course_com[i]:
+                dfs(i,[])
+            if course_com[i] == False:
+                return False
+        return True"""
+
+        adj_lis = defaultdict(list)
+        for pre in prerequisites :
+            adj_lis[pre[0]].append(pre[1])
+
+        visited = set()
+        path = set()
+        
+        def dfs(course, path):
+            if course in visited:
+                return True
+            elif course in path:
+                return False
+            else :
+                path.add(course)
+                for req in adj_lis[course]:
+                    if not dfs(req,path):
+                        return False
+                path.remove(course)
+                visited.add(course)
+                return True
+        
+        for course in range(numCourses):
+            if not dfs(course,path):
+                return False
+        return True
