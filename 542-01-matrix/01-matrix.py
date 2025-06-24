77e935e27a7bc0f5440bf5015fd53cdd06220129
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-24 18:06:06 -0400

    Time: 200 ms (28.21%) | Memory: 21.8 MB (16.66%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -3,7 +3,7 @@ class Solution:
         if not mat : return []
         m,n = len(mat), len(mat[0])
         ans = [[float('inf')]*n for _ in range(m)]
-        q = []
+        q = deque()
         visited = set()
         for i in range(m):
             for j in range(n):
@@ -12,7 +12,7 @@ class Solution:
                     q.append((i,j))
                     visited.add((i,j))
         while q:
-            x,y = q.pop(0)
+            x,y = q.popleft()
             for i,j in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]:
                 if 0 <= i < m and 0 <= j < n and (i,j) not in visited:
                     ans[i][j] = min(ans[i][j], 1+ans[x][y])
