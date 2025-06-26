648927ba585ffb0c7096fb6c538da086d9235de9
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-26 16:08:02 -0400

    Time: 5 ms (34.99%) | Memory: 17.7 MB (70.97%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,39 +2,29 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         minutes = 0
         m,n = len(grid), len(grid[0])
-        cnt_one,cnt_two = 0,0 
-        rotten_q = []
+        cnt_fresh,cnt_rotten = 0,0 
+        rotten_q = deque()
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1: cnt_one += 1
+                if grid[i][j] == 1: cnt_fresh += 1
                 if grid[i][j] == 2: 
-                    cnt_two += 1
+                    cnt_rotten += 1
                     rotten_q.append((i,j))
-        if cnt_one == 0 : return 0
-        if cnt_two == 0 : return -1
+        if cnt_fresh == 0 : return 0
+        if cnt_rotten == 0 : return -1
 
         while rotten_q:
             minutes += 1
             l = len(rotten_q)
             for _ in range(l):
-                xpos, ypos = rotten_q.pop(0)
-                if xpos > 0 and grid[xpos-1][ypos] == 1:
-                    grid[xpos-1][ypos] = 2
-                    rotten_q.append((xpos-1,ypos))
-                    cnt_one -= 1
-                if xpos < m-1 and grid[xpos+1][ypos] == 1:
-                    grid[xpos+1][ypos] = 2
-                    rotten_q.append((xpos+1,ypos))
-                    cnt_one -= 1
-                if ypos < n-1 and grid[xpos][ypos+1] == 1:
-                    grid[xpos][ypos+1] = 2
-                    rotten_q.append((xpos,ypos+1))
-                    cnt_one -= 1
-                if ypos > 0 and grid[xpos][ypos-1] == 1:
-                    grid[xpos][ypos-1] = 2
-                    rotten_q.append((xpos,ypos-1))
-                    cnt_one -= 1
-                if cnt_one == 0:
+                xpos, ypos = rotten_q.popleft()
+                for x,y in [(xpos-1,ypos),(xpos+1,ypos),(xpos,ypos-1),(xpos,ypos+1)]:
+                    if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
+                        grid[x][y] = 2
+                        rotten_q.append((x,y))
+                        cnt_fresh -= 1
+
+                if cnt_fresh == 0:
                     return minutes
 
-        if cnt_one : return -1
+        if cnt_fresh : return -1
