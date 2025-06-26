a55f353cb38ee16fb38b73839c5acd6ba364fb1f
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-06-26 16:14:02 -0400

    Time: 3 ms (82.01%) | Memory: 17.7 MB (92.76%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -20,6 +20,7 @@ class Solution:
                         rotten_q.append((x,y))
                         cnt_fresh -= 1
 
+                if cnt_fresh == 0:
+                    return minutes
+
         if cnt_fresh : return -1
-        else :
-            return minutes - 1
