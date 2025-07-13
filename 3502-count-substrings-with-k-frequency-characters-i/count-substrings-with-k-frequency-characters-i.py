c0d0a254070632d77be25c3822d7fb15c7cf10d7
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-07-13 02:26:57 -0400

    Time: 7 ms (84.17%) | Memory: 17.9 MB (19.78%) - LeetSync

--- 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.py
+++ 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.py
@@ -2,11 +2,20 @@ class Solution:
     def numberOfSubstrings(self, s: str, k: int) -> int:
         total = 0
         n = len(s)
-        for i in range(n-k+1):
+        """for i in range(n-k+1):
             freq = [0]*26
             for j in range(i,n):
                 freq[ord(s[j])-ord("a")] += 1
                 if freq[ord(s[j])-ord("a")] >= k:
                     total += n-j
-                    break
+                    break"""
+        freq = [0]*26
+        l = 0
+        for r in range(n):
+            idx = ord(s[r])-ord("a")
+            freq[idx] += 1
+            while freq[idx] >= k:
+                total += n-r
+                freq[ord(s[l])-ord("a")] -= 1
+                l += 1
         return total
