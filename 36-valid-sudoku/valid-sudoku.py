a80033b07e9bd3bddc6d1e390f8650d96cf07fac
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-07-15 20:15:38 -0400

    Time: 7 ms (37.09%) | Memory: 17.7 MB (69.42%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,35 +1,16 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for j in range(9):
-            # row condition
-            d = {}
-            for i in range(9):
-                if board[j][i] == '.':
-                    continue
-                elif board[j][i] in d:
-                    return False
-                else :
-                    d[board[j][i]] = 1
-            print(d)
-            # col condition
-            d = {}
-            for i in range(9):
-                if board[i][j] == '.':
-                    continue
-                elif board[i][j] in d:
-                    return False
-                else :
-                    d[board[i][j]] = 1
-        
+        row_hash = defaultdict(set)
+        col_hash = defaultdict(set)
+        grid_hash = defaultdict(set)
         for i in range(9):
-            d = {}
-            for j in range(3):
-                for k in range(3):
-                    if board[(i//3)*3 + j][(i%3)*3 + k] == '.':
-                        continue
-                    elif board[(i//3)*3 + j][(i%3)*3 + k] in d:
-                        return False
-                    else :
-                        d[board[(i//3)*3+j][(i%3)*3 + k]] = 1
-        
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                elif board[i][j] in row_hash[i] or board[i][j] in col_hash[j] or board[i][j] in grid_hash[(i//3,j//3)]:
+                    return False
+                else:
+                    row_hash[i].add(board[i][j])
+                    col_hash[j].add(board[i][j])
+                    grid_hash[(i//3,j//3)].add(board[i][j])
         return True
