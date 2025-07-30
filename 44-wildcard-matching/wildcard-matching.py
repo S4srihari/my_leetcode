0bd2f75df52ef85685c5d7591767fedeb48eea9b
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-07-30 19:10:59 -0400

    Time: 302 ms (74.43%) | Memory: 25.9 MB (46.76%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,9 +1,5 @@
 class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        m,n = len(s),len(p)
-        cache = {}
-
-        def dfs(i,j):
+    def dfs(i,j):
             if i == m and j == n:
                 return True
             if j == n:
@@ -25,6 +21,24 @@ class Solution:
             else:
                 cache[(i,j)] = False
                 return cache[(i,j)]
+
+    def isMatch(self, s: str, p: str) -> bool:
+        m,n = len(s),len(p)
+        dp = [[False]*(n+1) for _ in range(m+1)]
         
-        return dfs(0,0)
-        
+        dp[m][n] = True
+        for j in range(n-1,-1,-1):
+            if p[j] == "*":
+                dp[m][j] = dp[m][j+1]
+            else :
+                dp[m][j] = False
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if s[i] == p[j] or p[j] == '?':
+                    dp[i][j] = dp[i+1][j+1]
+                elif p[j] == "*":
+                    dp[i][j] =  dp[i][j+1] or dp[i+1][j]
+                else:
+                    dp[i][j] = False
+        return dp[0][0]
