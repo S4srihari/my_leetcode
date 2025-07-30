7ab6192421df30a2cae16f67bdcdc1f604c81f98
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-07-30 19:19:29 -0400

    Time: 247 ms (81.16%) | Memory: 17.8 MB (94.71%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,43 +1,23 @@
 class Solution:
-    def dfs(i,j):
-            if i == m and j == n:
-                return True
-            if j == n:
-                return False
-            if i == m:
-                while j < n:
-                    if p[j] != "*":
-                        return False
-                    else :
-                        j += 1
-                return True
-            if (i,j) in cache: return cache[(i,j)]
-            if s[i] == p[j] or p[j] == '?':
-                cache[(i,j)] = dfs(i+1,j+1)
-                return cache[(i,j)]
-            elif p[j] == "*":
-                cache[(i,j)] =  dfs(i,j+1) or dfs(i+1,j)
-                return cache[(i,j)]
-            else:
-                cache[(i,j)] = False
-                return cache[(i,j)]
-
     def isMatch(self, s: str, p: str) -> bool:
         m,n = len(s),len(p)
-        dp = [[False]*(n+1) for _ in range(m+1)]
+        prev = [False]*(n+1)
+        cur = [False]*(n+1)
         
-        dp[m][n] = True
+        prev[n] = True
         j = n-1
         while j > -1 and p[j] == "*":
-            dp[m][j] = True
+            prev[j] = True
             j -=1 
 
         for i in range(m-1,-1,-1):
+            cur[n] = False
             for j in range(n-1,-1,-1):
                 if s[i] == p[j] or p[j] == '?':
-                    dp[i][j] = dp[i+1][j+1]
+                    cur[j] = prev[j+1]
                 elif p[j] == "*":
-                    dp[i][j] =  dp[i][j+1] or dp[i+1][j]
+                    cur[j] =  cur[j+1] or prev[j]
                 else:
-                    dp[i][j] = False
-        return dp[0][0]
+                    cur[j] = False
+            prev,cur = cur,prev
+        return prev[0]
