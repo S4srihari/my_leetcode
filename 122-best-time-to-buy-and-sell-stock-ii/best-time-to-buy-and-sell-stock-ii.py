b29b0f880203f6e7b7f21f73ed542e37c2722b64
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-08-02 11:43:35 -0400

    Time: 11 ms (10.22%) | Memory: 21.2 MB (7.39%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -4,15 +4,23 @@ class Solution:
             return 0
         if dp[idx][can_buy] != -1: return dp[idx][can_buy]
         if can_buy :
-            dp[idx][can_buy] = max(self.profit(idx+1,n,can_buy,prices,dp),self.profit(idx+1,n,not can_buy,prices,dp)-prices[idx])
+            dp[idx][can_buy] = max(self.profit(idx+1,n,can_buy,prices,dp),self.profit(idx+1,n,0,prices,dp)-prices[idx])
             return dp[idx][can_buy]
         else:
-            dp[idx][can_buy] = max(self.profit(idx+1,n,can_buy,prices,dp),self.profit(idx+1,n,not can_buy,prices,dp)+prices[idx])
+            dp[idx][can_buy] = max(self.profit(idx+1,n,can_buy,prices,dp),self.profit(idx+1,n,1,prices,dp)+prices[idx])
             return dp[idx][can_buy]
 
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         if n<2:
             return 0
-        dp = [[-1]*2 for _ in range(n)]
-        return self.profit(0,n,True,prices,dp) 
+        dp = [[-1]*2 for _ in range(n+1)]
+        dp[n][0],dp[n][1] = 0,0
+        for idx in range(n-1,-1,-1):
+            for can_buy in range(2):
+                if can_buy :
+                    dp[idx][can_buy] = max(dp[idx+1][can_buy],dp[idx+1][0]-prices[idx])
+                else:
+                    dp[idx][can_buy] = max(dp[idx+1][can_buy],dp[idx+1][1]+prices[idx])
+
+        return dp[0][1]
