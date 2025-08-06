595eb2da87a7be2c8ac4d900617f0441e21ef2a2
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-08-06 16:54:24 -0400

    Time: 2 ms (76.82%) | Memory: 25.5 MB (92.35%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,14 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #Using Extra Space
-        """k %= len(nums)
-        temp = nums[-k:] + nums[:-k]
-        for idx in range(len(nums)):
-            nums[idx] = temp[idx]"""
         n = len(nums)
         k %= n
-        if k == 0: return
-        nums[-k:] = nums[-k:][::-1]
-        nums[:n-k] = nums[:n-k][::-1]
-        nums[:] = nums[:][::-1]
+        if k == 0 : return 
+        nums[:] = nums[-k:]+nums[:n-k]
