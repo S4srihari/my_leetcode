1afffa392006667718ace864af3a6086abcefcf6
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-08-09 11:51:08 -0400

    Time: 487 ms (44.30%) | Memory: 18.1 MB (57.83%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.py
+++ 282-expression-add-operators/expression-add-operators.py
@@ -1,50 +1,23 @@
 class Solution:
-    def value_equals_target(self,lis,target):
-        res = []
-        for equation in lis:
-            stack = []
-            num = 0
-            sign = '+'
-            temp = deque(equation)
-            while temp:
-                char = temp.popleft()
-                if char.isdigit():
-                    num = num * 10 + int(char)
-                if char in '+-*' or not temp:
-                    if sign == '+':
-                        stack.append(num)
-                    elif sign == '-':
-                        stack.append(-num)
-                    elif sign == '*':
-                        stack[-1] = stack[-1] * num
-                    sign = char
-                    num = 0
-
-            if sum(stack) == target:
-                res.append(equation)
-        return res
-            
     def addOperators(self, num: str, target: int) -> List[str]:
-        if len(num) == 1 and int(num) == target:
-            return [num]
-        elif len(num) == 1:
-            return []
-        store_pos = []
-        def operation(num,cur):
-            operators = ['*','+','-','']
-            if not num:
-                store_pos.append(cur)
+        n = len(num)
+        res = []
+        if n == 0: return res
+        def canExpress(idx,path,cur,prev):
+            if idx == len(num):
+                if cur == target:
+                    res.append(path)
                 return
-            temp = cur
-            for op in operators:
-                if op == '':
-                    last_num = cur.split('+')[-1].split('-')[-1].split('*')[-1]
-                    if last_num == '0':
-                        continue
-                cur = cur + op + num[0]
-                operation(num[1:], cur)
-                cur = temp
-                    
-        operation(num[1:],num[0])
-        
-        return  self.value_equals_target(store_pos,target)
+            
+            for i in range(idx,n):
+                if i > idx and num[idx] == '0': break
+                val = int(num[idx:i+1])
+                if idx  == 0:
+                    canExpress(i+1,path+str(val),cur+val,val)
+                else:
+                    canExpress(i+1,path+"+"+str(val),cur+val,val)
+                    canExpress(i+1,path+"-"+str(val),cur-val,-val)
+                    canExpress(i+1,path+"*"+str(val),cur-prev + (prev*val), prev*val)
+            
+        canExpress(0,"",0,0)
+        return res
