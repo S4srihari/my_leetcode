ccec01ec81ca21281b894975b4673acf82cf10b1
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-08-21 18:58:57 -0400

    Time: 39 ms (91.36%) | Memory: 34.6 MB (17.90%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -3,18 +3,17 @@ class Solution:
         res = []
         n = len(s)
 
-        def palindrome(i,j):
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i,j = i+1,j-1
-            return True
+        is_pal = [[False]*n for _ in range(n)]
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i <= 2 or is_pal[i+1][j-1]):
+                    is_pal[i][j] = True
 
         def backtrack(idx,cur):
             if idx == n:
                 res.append(cur[:])
             for i in range(idx,n):
-                if palindrome(idx,i):
+                if is_pal[idx][i]:
                     cur.append(s[idx:i+1])
                     backtrack(i+1,cur)
                     cur.pop()
