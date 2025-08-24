72c538217e0b8b4eb68d566168f5ac4af2905cb0
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-08-24 19:28:39 -0400

    Time: 151 ms (6.02%) | Memory: 55.2 MB (57.25%) - LeetSync

---
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        check = [False]*3
+        for triplet in triplets:
+            if any([triplet[i] > target[i] for i in range(3)]):
+                continue
+            for i in range(3):
+                if triplet[i] == target[i]: check[i] = True
+        return all(check)
