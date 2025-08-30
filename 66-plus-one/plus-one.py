abc5952ba2fd7471d3938e32b174c420aa83003a
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-08-30 15:09:45 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.93%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,6 +4,5 @@ class Solution:
             if digits[i] != 9:
                 digits[i] += 1
                 return digits
-            else:
-                digits[i] = 0
+            digits[i] = 0
         return [1]+digits
