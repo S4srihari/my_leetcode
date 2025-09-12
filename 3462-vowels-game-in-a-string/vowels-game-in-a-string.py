4b3d1debae35a71561192b68c1ec5f8d9cb085ec
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-09-12 09:17:38 -0400

    Time: 3 ms (86.06%) | Memory: 18.3 MB (31.82%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -1,6 +1,8 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-        for c in s:
+        """for c in s:
             if c in "aeiou":
                 return True
-        return False
+        return False"""
+
+        return s.count("a") + s.count("e") + s.count("i") + s.count("o") + s.count("u") > 0
