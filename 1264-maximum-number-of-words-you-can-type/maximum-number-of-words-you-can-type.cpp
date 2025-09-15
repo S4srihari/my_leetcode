3b23db8731e7f9b1d385f09108eb6b928edca889
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-09-15 00:40:12 -0400

    Time: 2 ms (27.91%) | Memory: 8.9 MB (79.40%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        int res = 0;
         unordered_set<char> broken(brokenLetters.begin(), brokenLetters.end());
-        stringstream words(text);
-        string word;
-        while (words >> word){
-            bool canType = true;
-            for (char c : word){
-                if (broken.count(c)){
-                    canType = false;
-                    break;
+        int count = 0;
+        string word = "";
+
+        for (int i = 0; i <= text.size(); i++) {
+            if (i == text.size() || text[i] == ' ') {
+                bool canType = true;
+                for (char c : word) {
+                    if (broken.count(c)) {
+                        canType = false;
+                        break;
+                    }
                 }
+                if (canType && !word.empty()) count++;
+                word.clear();
+            } else {
+                word.push_back(text[i]);
             }
-            if(canType) res++;
         }
-        return res;
-    }
+        return count;
+        }
 };
