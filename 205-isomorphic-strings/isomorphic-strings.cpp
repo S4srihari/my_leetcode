e5dbdc875ae0b32eb61f92f041c896059e3cf0df
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-09-16 18:16:22 -0400

    Time: 0 ms (100.00%) | Memory: 9.6 MB (8.68%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,17 +4,13 @@ public:
         if (s.size()!= t.size()) return false;
         unordered_map<char,char> smap, tmap;
         for(int i=0; i <s.size(); i++){
-            char c1 = s[i], c2 = t[i];
-            if (smap.count(c1)) {
-                if (smap[c1] != c2) return false;
+            if (smap.find(s[i]) == smap.end()) {
+                smap[s[i]] = i;
             } 
-            else if (tmap.count(c2)) {
-                if (tmap[c2] != c1) return false;
+            if (tmap.find(t[i])==tmap.end()) {
+                tmap[t[i]] = i;  
             } 
-            else {
-                smap[c1] = c2;
-                tmap[c2] = c1;
-            }
+            if(smap[s[i]] != tmap[t[i]]) return false;
         }
         return true;
     }
