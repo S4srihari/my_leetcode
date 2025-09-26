522e729f907a9af3747457c83eb7c58ff27442bb
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-09-26 00:14:06 -0400

    Time: 30 ms (73.93%) | Memory: 16.7 MB (42.88%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -3,12 +3,17 @@ public:
     int triangleNumber(vector<int>& nums) {
         int cnt = 0, n = nums.size();
         sort(nums.begin(),nums.end());
-        for (int i=0; i<n-2; i++){
-            if (nums[i] <= 0) continue;
-            for(int j=i+1; j<n-1; j++){
-                auto val = std::upper_bound(nums.begin(), nums.end(), nums[i]+nums[j]-1);
-                int idx = std::distance(nums.begin(), val);
-                cnt += idx-j-1;
+        for(int i=n-1; i>1; i--){
+            int r=i-1;
+            int l=0;
+            while(l<r){
+                int sum=nums[l]+nums[r];
+                if(sum>nums[i]){
+                    cnt += r-l;
+                    r--;
+                }else{
+                   l++;
+                }
             }
         }
         return cnt;
