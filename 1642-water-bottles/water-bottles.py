24c4ccac9f02ea83846ffa6af84a0ffec2ddf000
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-09-30 22:27:32 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (17.52%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -1,5 +1,6 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        if numExchange < 2: return -1
         cnt, bonus = 0,0
         while(numBottles >= numExchange):
             bonus = numBottles//numExchange
