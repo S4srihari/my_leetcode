3e28282b1c2ca7a72114bcfb095e11e130eec65b
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-10-04 12:38:19 -0400

    Time: 3 ms (37.29%) | Memory: 11.8 MB (20.31%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -5,9 +5,7 @@ public:
         st.push(-1);
         int res = 0;
         for(int i=0; i<s.size(); i++){
-            if (s.at(i) == '(') {
-                st.push(i);
-            }
+            if (s.at(i) == '(') st.push(i);
             else {
                 st.pop();
                 if (st.empty()) st.push(i);
