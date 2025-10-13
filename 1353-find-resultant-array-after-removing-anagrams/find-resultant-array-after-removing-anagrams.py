f34cfb61d39bf0fc0e56ec80af57f6a46d806ee2
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-10-12 20:19:22 -0400

    Time: 15 ms (29.35%) | Memory: 18 MB (28.68%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -3,10 +3,17 @@ class Solution:
         if not words: return []
         res = []
         res.append(words[0])
-        prev = Counter(words[0])
+
+        def compare(a,b):
+            freq = [0]*26
+            for c in a:
+                freq[ord(c)-ord('a')] += 1
+            for c in b:
+                freq[ord(c) - ord('a')] -= 1
+            return all(j == 0 for j in freq)
+
         for i in range(1,len(words)):
-            cur = Counter(words[i])
-            if prev != cur:
+            if not compare(res[-1],words[i]):
                 res.append(words[i])
-                prev = cur
+                
         return res
