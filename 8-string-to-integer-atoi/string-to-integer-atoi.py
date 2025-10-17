4cea21f2931f60efbd77dea850968a6648a0a170
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-10-17 10:56:05 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (49.29%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,25 +1,21 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        st = s.strip()
-        res = 0
-        sign = 1
-        seq = False
-        valid = ['0','1','2','3','4','5','6','7','8','9','-','+']
-        for c in st:
-            if c not in valid:
-                break
-            elif (c == '-' or c =='+') and (sign == -1 or seq):
-                break
-            elif c == '-':
-                sign = -1
-                seq = True
-            elif c == '+':
-                sign = 1
-                seq = True
-            else:
-                res *= 10 
-                res += int(c)
-                seq = True
-        if res >= 2**31 and sign == -1: return -2**31
-        elif res > 2**31-1 : return 2**31 - 1
-        return res*sign
+        s=s.lstrip()
+        if not s:
+            return 0
+        sign=1
+        i=0
+        if s[0] in['+','-']:
+            sign=-1 if s[0]=='-' else 1
+            i+=1
+        num=0
+        while i<len(s)and s[i].isdigit():
+            num=num*10+int(s[i])
+            i+=1
+        num*=sign
+        INT_MIN,INT_MAX= -2**31,2**31 -1
+        if num <INT_MIN:
+            return INT_MIN
+        elif num >INT_MAX:
+            return INT_MAX
+        return num
