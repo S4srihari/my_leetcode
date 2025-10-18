7afb12a376604bb3dda26cf63e4938dd3a0ee73c
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-10-18 00:18:18 -0400

    Time: 763 ms (42.01%) | Memory: 30.6 MB (100.00%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.py
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.py
@@ -1,16 +1,5 @@
 class Solution:
     def maxDistinctElements(self, nums: List[int], k: int) -> int:
-        """freqMap = Counter(nums)
-        hashSet = set()
-        for ele in sorted(freqMap.keys()):
-            val = freqMap[ele]
-            diff = -k
-            while diff <= k and val > 0:
-                if ele+diff not in hashSet:
-                    hashSet.add(ele+diff)
-                    val -= 1
-                diff += 1
-        return len(hashSet)"""
         if not nums : return 0
         nums.sort()
         nums[0] -= k
@@ -21,6 +10,5 @@ class Solution:
                 nums[i] = max(cur+1, nums[i]-k)
                 res += 1
                 cur = nums[i]
-                print(cur)
         return res
             
