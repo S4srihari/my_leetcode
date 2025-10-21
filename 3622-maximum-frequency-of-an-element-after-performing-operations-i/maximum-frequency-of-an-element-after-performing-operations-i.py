2e35694e59ef872768c39e1249bec84bbd614e20
Srihari <85178734+S4srihari@users.noreply.github.com>
2025-10-20 23:42:51 -0400

    Time: 591 ms (63.37%) | Memory: 33.4 MB (62.21%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.py
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.py
@@ -2,7 +2,6 @@ class Solution:
     def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
         c = Counter(nums)
         nums.sort()
-        n = len(nums)
         res = 0
         for i in range(nums[0], nums[-1]+1):
             left = bisect_left(nums, i-k)
