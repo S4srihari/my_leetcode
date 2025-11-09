7c13903053cec52e47534901e39593303c3cd438
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-11-08 19:51:03 -0500

    Time: 47 ms (73.39%) | Memory: 34.2 MB (67.42%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,32 +1,12 @@
-class unionFind:
-    def __init__(self):
-        self.parent = {}
-
-    def find(self,num):
-        if self.parent[num] != num:
-            self.parent[num] = self.find(self.parent[num])
-        return self.parent[num]
-    
-    def union(self,num1, num2):
-        self.parent[num2] = num1
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        visEle = set()
-        uf = unionFind()
-        for num in nums:
-            if num+1 in visEle:
-                uf.union(num,num+1)
-            if num-1 in visEle:
-                uf.union(num-1,num)
-            else:
-                uf.union(num,num)
-            visEle.add(num)
-        
-        cnt =  defaultdict(int)
+        store = set(nums)
         res = 0
-        for num in visEle:
-            grp = uf.find(num)
-            cnt[grp] += 1
-            res = max(res,cnt[grp])
+        for num in store:
+            if num-1 in store:
+                continue
+            k = 0
+            while num+k in store:
+                k += 1
+            res = max(res,k)
         return res
