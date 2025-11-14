965890e2070afd8138fa50bd776698e829739223
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-11-13 22:31:25 -0500

    Time: 179 ms (66.20%) | Memory: 38.2 MB (58.69%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.py
@@ -2,13 +2,19 @@ class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
         mat = [[0]*n for _ in range(n)]
         for r1,c1,r2,c2 in queries:
-            for r in range(r1,r2+1):
-                mat[r][c1] += 1
-                if c2 < n-1: mat[r][c2+1] -= 1
+            mat[r1][c1] += 1
+            if c2 < n-1: mat[r1][c2+1] -= 1
+            if r2 < n-1: mat[r2+1][c1] -= 1
+            if r2 < n-1 and c2 < n-1: mat[r2+1][c2+1] += 1
         
         #Horizontal
         for i in range(n):
             for j in range(1,n):
                 mat[i][j] = mat[i][j] + mat[i][j-1]
+
+        #Vertical
+        for j in range(n):
+            for i in range(1,n):
+                mat[i][j] += mat[i-1][j]
         
         return mat
