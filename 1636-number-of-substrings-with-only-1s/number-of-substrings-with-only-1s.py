da61357d1401d7aae8e65e9886bd53178b324e54
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-11-15 23:57:08 -0500

    Time: 31 ms (15.36%) | Memory: 18.2 MB (40.82%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
@@ -5,18 +5,12 @@ class Solution:
         n = len(s)
         st =  0
         for i in range(n):
-            if s[i] != '1':
-                if i > 0 and s[i-1] == '1':
+            if s[i] == '0' and i > 0 and s[i-1] == '1':
                     k = i-st
                     res += (k * (k+1))//2
                     res %= mod
-                else :
-                    continue
-            else :
-                if i > 0 and s[i-1] == '0':
+            elif i > 0 and s[i-1] == '0':
                     st = i
-                else:
-                    continue
         if n > 1 and s[n-1] == '1':
             k = n-st
             res += k*(k+1)//2
