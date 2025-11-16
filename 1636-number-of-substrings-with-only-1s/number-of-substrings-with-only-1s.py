8a2e46312de26939422b6b680ee8db4e70465f82
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-11-15 23:54:20 -0500

    Time: 29 ms (18.35%) | Memory: 18.3 MB (40.82%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        res = 0
+        mod = 10**9 +  7
+        n = len(s)
+        st =  0
+        for i in range(n):
+            if s[i] != '1':
+                if i > 0 and s[i-1] == '1':
+                    k = i-st
+                    res += (k * (k+1))//2
+                    res %= mod
+                else :
+                    continue
+            else :
+                if i > 0 and s[i-1] == '0':
+                    st = i
+                else:
+                    continue
+        if n > 1 and s[n-1] == '1':
+            k = n-st
+            res += k*(k+1)//2
+        return res
+                
