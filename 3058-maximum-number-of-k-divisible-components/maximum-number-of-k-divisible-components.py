0b8a65009e38f73a215bfc249b6c6c2b9317b9d2
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-11-27 22:57:19 -0500

    Time: 119 ms (64.49%) | Memory: 41.3 MB (23.91%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
@@ -4,16 +4,15 @@ class Solution:
         for u,v in edges:
             adj_list[u].append(v)
             adj_list[v].append(u)
-        comps = 1
+        self.comps = 1
         def dfs(node,parent):
-            nonlocal comps
             cur = values[node]
             for adj_node in adj_list[node]:
                 if adj_node != parent:
                     val = dfs(adj_node, node)
                     if val%k == 0:
-                        comps += 1
+                        self.comps += 1
                     cur += val
             return cur
         dfs(0,-1)
-        return comps
+        return self.comps
