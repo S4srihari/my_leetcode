f309569fe3f1cc741c4e07ba628d29b630bbf48b
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-11-29 21:06:52 -0500

    Time: 71 ms (82.87%) | Memory: 38.3 MB (27.62%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -1,19 +1,16 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        preSum = []
-        tot, n = 0, 0
-        for num in nums:
-            tot += num
-            preSum.append(tot)
-            n += 1
-        if tot < p: return -1
-        res, rem = n, tot%p 
+        s = sum(nums)
+        if s < p: return -1
+        if s%p == 0: return 0
+        n = len(nums)
+        preSum = 0
+        res, rem = n, s%p
         hashMap = {}
         hashMap[0] = -1
-        if rem == 0: return 0
         for i in range(n):
-            curRem = preSum[i]%p
-            if nums[i]%p == rem: return 1
+            preSum += nums[i]
+            curRem = preSum%p
             needed = (curRem - rem)%p
             if needed in hashMap:
                 res = min(res, i-hashMap[needed])
