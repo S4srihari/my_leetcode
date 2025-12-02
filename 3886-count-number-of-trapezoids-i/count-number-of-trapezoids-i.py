e9c334207260dd25fc386db9cd7393a64f93e30f
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-12-01 23:40:57 -0500

    Time: 103 ms (12.09%) | Memory: 63.5 MB (72.53%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
@@ -1,19 +1,17 @@
 class Solution:
     def countTrapezoids(self, points: List[List[int]]) -> int:
         mod = 10**9 + 7
-        lines = defaultdict(set)
+        lines = defaultdict(int)
         for x,y in points:
-            lines[y].add(x)
+            lines[y] += 1
         cntlines = []
+        tot = 0
         for line in lines:
-            cords = len(lines[line]) 
-            if cords > 1:
-                cntlines.append(cords)
-        if len(cntlines) <= 1:
-            return 0
+            cords = lines[line]
+            sides = (cords*(cords-1))//2
+            tot += sides
+            cntlines.append(sides)
         res = 0
-        cntlines = [(i*(i-1))//2 for i in cntlines]
-        tot = sum(cntlines)
         for cnt in cntlines:
             res += (tot-cnt)*cnt
         res = res//2
