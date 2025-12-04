8b7c7776cbfabb6211907490f774a504065aafdb
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-12-04 00:09:06 -0500

    Time: 3 ms (98.88%) | Memory: 18.3 MB (56.88%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.py
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.py
@@ -1,21 +1,4 @@
 class Solution:
     def countCollisions(self, directions: str) -> int:
-        r_cnt = 0
-        stay = False
-        res = 0
-        for car in directions:
-            if car == "S":
-                if r_cnt > 0:
-                    res += r_cnt
-                    r_cnt = 0
-                stay = True
-            elif car == "L":
-                if r_cnt > 0:
-                    res += 1+r_cnt
-                    stay = True
-                    r_cnt = 0
-                elif stay:
-                    res += 1
-            else:
-                r_cnt += 1
-        return res
+        collisions = directions.lstrip('L').rstrip('R')
+        return len(collisions) - collisions.count("S") 
