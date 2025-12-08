a3f248a94d33c07bd51fd293a40a66bb8fc37835
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-12-07 19:28:16 -0500

    Time: 71 ms (90.22%) | Memory: 18 MB (11.68%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.py
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -1,10 +1,12 @@
 class Solution:
     def countTriples(self, n: int) -> int:
         cnt = 0
-        for i in range(1,n):
-            cur = i**2
-            for j in range(1, n):
-                val = (cur + j**2)**0.5
-                if val  <= n and val%1 == 0:
-                    cnt += 1
+        squares = set()
+        for i in range(1,n+1):
+            squares.add(i**2)
+        limit = int(((n**2)/2)**0.5)
+        for i in range(1,limit+1):
+            for j in range(i+1,n+1):
+                if i**2 + j**2 in squares:
+                    cnt += 2
         return cnt
