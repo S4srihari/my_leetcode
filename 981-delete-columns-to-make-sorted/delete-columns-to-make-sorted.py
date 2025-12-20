9f5e02fa155c53e857465665a888f9a43d68fc03
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-12-19 23:04:06 -0500

    Time: 63 ms (59.77%) | Memory: 18.2 MB (73.28%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
@@ -3,10 +3,9 @@ class Solution:
         n = len(strs)
         m = len(strs[0])
         res = [False]*m
-        cur = [i for i in strs[0]]
-        for i in range(1,n):
-            for j in range(m):
-                if strs[i][j] < cur[j]:
+        for j in range(m):
+            for i in range(1,n):
+                if strs[i][j] < strs[i-1][j]:
                     res[j] = True
-                cur[j] = strs[i][j]
+                    break
         return sum(res)
