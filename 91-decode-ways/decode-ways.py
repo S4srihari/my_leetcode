2e9800e8e4f88eddb11e55e5833c27157aea3c98
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-12-22 13:02:44 -0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (99.08%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,18 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        res = [1]
-        if s[0] == "0" : return 0
-        for i in range(1,len(s)):
-            if s[i] == "0" and s[i-1] not in "12":
-                return 0
-            elif s[i] == "0" or (i+1 < len(s) and s[i+1] == "0"):
-                res.append(res[-1])
-            elif s[i-1] == "1":
-                if res[-1] == 1: res.append(2)
-                else : res.append(res[-1]+res[-2])
-            elif s[i-1] == "2" and s[i] in "123456":
-                if res[-1] == 1: res.append(2)
-                else : res.append(res[-1]+res[-2])
-            else :
-                res.append(res[-1])
-        return res[-1]
+        n = len(s)
+        dp = [-1]*n
+        def recurse(idx):
+            if idx == n:
+                return 1
+            if dp[idx] != -1:
+                return dp[idx]
+            #single
+            take1,take2 = 0,0
+            if 1<= int(s[idx]) <= 26:
+                take1 = recurse(idx+1)
+            #double
+            if idx <= n-2 and 10<=int(s[idx:idx+2])<=26 :
+                take2 = recurse(idx+2)
+            dp[idx] = take1 + take2
+            return dp[idx]
+        return recurse(0)
