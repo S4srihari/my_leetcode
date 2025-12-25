37f40309511f102ee9e1d3e4758b187b0d6bb9d9
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-12-24 23:54:56 -0500

    Time: 938 ms (9.93%) | Memory: 43.5 MB (92.20%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -1,7 +1,8 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        happiness.sort()
+        happiness = [i*(-1) for i in happiness]
+        heapq.heapify(happiness)
         res = 0
         for i in range(k):
-            res += max(0,happiness.pop() - i)
+            res += max(0, heapq.heappop(happiness)*(-1) - i)
         return res
