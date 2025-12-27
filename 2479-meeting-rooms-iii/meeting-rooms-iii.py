3e14533fc4f50e32890fb361a3b1ca84dfe17589
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-12-26 22:53:30 -0500

    Time: 775 ms (5.93%) | Memory: 51.1 MB (94.93%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -1,13 +1,12 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         # finding the roon that is used the most
-        # first meeting always starts in first room
+        # meeting always helds in first room available
         meetings.sort()
         room_meetings = [0]*n
         next_available_time = [0]*n
-        heapq.heapify(next_available_time)
         for start,end in meetings:
-            ends_fast = 0
+            min_idx = 0
             executed = False
             for i in range(n):
                 if next_available_time[i] <= start :
@@ -15,16 +14,16 @@ class Solution:
                     next_available_time[i] = end
                     executed = True
                     break
-                elif next_available_time[i] < next_available_time[ends_fast]:
-                    ends_fast = i
+                elif next_available_time[i] < next_available_time[min_idx]:
+                    min_idx = i
             if not executed:
-                room_meetings[ends_fast] += 1
-                next_available_time[ends_fast] += end - start
+                room_meetings[min_idx] += 1
+                next_available_time[min_idx] += end - start
 
-        res = 0
+        res_room = 0
         max_meets = 0
         for i in range(n):
             if room_meetings[i] > max_meets:
-                res = i
+                res_room = i
                 max_meets = room_meetings[i]
-        return res
+        return res_room
