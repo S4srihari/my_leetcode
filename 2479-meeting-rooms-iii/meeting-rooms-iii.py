3d79f310b57701d369d9a13501353dfc75d4f03d
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2025-12-26 22:59:09 -0500

    Time: 797 ms (5.12%) | Memory: 50.9 MB (96.13%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -3,27 +3,25 @@ class Solution:
         # finding the roon that is used the most
         # meeting always helds in first room available
         meetings.sort()
-        room_meetings = [0]*n
-        next_available_time = [0]*n
+        count = [0]*n
+        ready_at = [0]*n
         for start,end in meetings:
             min_idx = 0
-            executed = False
             for i in range(n):
-                if next_available_time[i] <= start :
-                    room_meetings[i] += 1
-                    next_available_time[i] = end
-                    executed = True
-                    break
-                elif next_available_time[i] < next_available_time[min_idx]:
+                if ready_at[i] < ready_at[min_idx]:
                     min_idx = i
-            if not executed:
-                room_meetings[min_idx] += 1
-                next_available_time[min_idx] += end - start
+                if ready_at[i] <= start :
+                    count[i] += 1
+                    ready_at[i] = end
+                    break
+            else:
+                count[min_idx] += 1
+                ready_at[min_idx] += end - start
 
         res_room = 0
         max_meets = 0
         for i in range(n):
-            if room_meetings[i] > max_meets:
+            if count[i] > max_meets:
                 res_room = i
-                max_meets = room_meetings[i]
+                max_meets = count[i]
         return res_room
