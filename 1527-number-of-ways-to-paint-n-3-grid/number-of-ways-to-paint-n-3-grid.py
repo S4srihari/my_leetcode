e6d77688340798009b53af02bdb8d395ab7a536c
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2026-01-02 22:15:45 -0500

    Time: 43 ms (27.44%) | Memory: 18.5 MB (20.73%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.py
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.py
@@ -1,20 +1,19 @@
 class Solution:
-    def solve(self, end, cur, form, dp):
-        mod = 10**9 +7
+    def solve(self, end, cur, form,dp):
+        mod = 10**9 + 7
         if cur >= end:
-            if form == 1:
+            if form == 1: # Type 1
                 return 5
-            else:
+            else:   # type 2
                 return 4
-        if dp[cur][form-1] != -1 :
+        if dp[cur][form-1] != -1:
             return dp[cur][form-1]
         if form == 1:
             dp[cur][form-1] = (self.solve(end, cur+1, 1, dp)*3 + self.solve(end, cur+1, 2, dp)*2)%mod
             return dp[cur][form-1]
         else:
-            dp[cur][form-1] = (self.solve(end, cur+1, 1, dp)*2 + self.solve(end, cur+1, 2, dp)*2)%mod
+            dp[cur][form-1] = (self.solve(end,cur+1, 1, dp)*2 + self.solve(end, cur+1, 2, dp)*2)%mod
             return dp[cur][form-1]
-
 
     def numOfWays(self, n: int) -> int:
         # First Row
@@ -24,26 +23,28 @@ class Solution:
         # Evaluate both cases from next rows
         # Type 1:  3 Type 1 and 2 Type 2
         # R Y R -> prev
-        # Y R G
-        #     Y
-        #   G Y
-        # G R G
-        #     Y
+        # Y R G -> 2
+        #     Y -> 1
+        #   G Y -> 1
+        # G R G -> 1
+        #     Y -> 2
 
         # Type 2: 2 Type 1 and 2 Type 2
         # R Y G ->  Prev
-        # Y G R
-        #     Y 
-        #   R Y
-        # G R Y
+        # Y G R -> 2
+        #     Y -> 1
+        #   R Y -> 1
+        # G R Y -> 2
 
-        mod = 10**9 + 7
         if n == 1:
             return 12
         tot = 0
+        mod = 10**9 + 7
         dp = [[-1]*2 for i in range(n)]
-        tot += 6*self.solve(n-1, 1, 1, dp) 
+        tot += self.solve(n-1, 1, 1, dp)*6
         tot %= mod
-        tot += 6*self.solve(n-1, 1 ,2, dp)
+        tot += self.solve(n-1, 1, 2, dp)*6
         tot %= mod
         return tot
+
+
