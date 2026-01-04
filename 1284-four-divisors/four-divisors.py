c07a08d1ba149e8fd9b02f1801880056d6bfbd4c
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2026-01-04 01:33:04 -0500

    Time: 1285 ms (5.23%) | Memory: 22.7 MB (5.23%) - LeetSync

--- 1284-four-divisors/four-divisors.py
+++ 1284-four-divisors/four-divisors.py
@@ -12,9 +12,16 @@ class Solution:
         return divisors
 
     def sumFourDivisors(self, nums: List[int]) -> int:
-        tot = 0
-        for num in nums:
-            divs = self.get_divisors(num)
-            if len(divs) == 4:
-                tot += sum(divs)
-        return tot
+        m = max(nums)
+        divs = [0 for i in range(m+1)]
+        sums = [0 for i in range(m+1)]
+        for i in range(1,m+1,1):
+            for j in range(i,m+1,i):
+                divs[j]+=1
+                sums[j]+=i
+        res = 0
+        for i in nums:
+            if divs[i] == 4:
+                res+=sums[i]
+        
+        return res
