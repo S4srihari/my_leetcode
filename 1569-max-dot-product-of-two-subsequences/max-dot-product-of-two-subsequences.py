4c59b9ff7fc244c79199458c9f2d6c0d31a8fd7b
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2026-01-08 01:13:51 -0500

    Time: 945 ms (5.03%) | Memory: 220.8 MB (5.53%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
@@ -1,19 +1,13 @@
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
         @lru_cache(None)
-        def solve(idx1, idx2, buf, flag):
+        def solve(idx1, idx2, flag):
             if idx1 >= len(nums1)  or idx2 >= len(nums2):
                 return 0 if flag else float("-inf")
-        
-            if buf is None:
-                take = solve(idx1, idx2, nums1[idx1], flag)
-                not_take = solve(idx1 + 1, idx2, buf, flag)
-                return max(take, not_take)
                 
-            else:
-                take = solve(idx1 + 1, idx2 +1, None, 1) + buf*nums2[idx2]
-                not_take = solve(idx1, idx2+1, buf, flag)
-                return max(take, not_take)
+            take = solve(idx1 + 1, idx2 +1, 1) + nums1[idx1]*nums2[idx2]
+            not_take = max(solve(idx1, idx2+1, flag), solve(idx1+1, idx2, flag))
+            return max(take, not_take)
 
 
-        return solve(0, 0, None, 0)
+        return solve(0, 0, 0)
