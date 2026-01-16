9ef25d158329e184d973f8237f02c4fb332eb514
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2026-01-15 22:18:35 -0500

    Time: 1618 ms (18.29%) | Memory: 40.7 MB (77.44%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.py
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.py
@@ -3,21 +3,20 @@ class Solution:
         mod = 10**9 + 7
         if m == n:
             return (m-1)*(n-1) % mod
-        hor_dis = set()
-        ver_dis = set()
+        heights = set()
         hors = [i for i in hFences] + [1,m]
         hors.sort()
         vers = [i for i in vFences] + [1,n]
         vers.sort()
+        res = -1
         for i in range(len(hors)):
             for j in range(i+1,len(hors)):
-                hor_dis.add(abs(hors[i]-hors[j]))
+                heights.add(abs(hors[i]-hors[j]))
         for i in range(len(vers)):
             for j in range(i+1,len(vers)):
-                ver_dis.add(abs(vers[i]-vers[j]))
-        res = -1
-        for x in hor_dis:
-            if x !=0 and x in ver_dis:
-                res = max(res, (x**2))
+                width = abs(vers[i]-vers[j])
+                if width in heights:
+                    res = max(res, width**2)
+        
         return res%mod if res > 0 else -1
 
