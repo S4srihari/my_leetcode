6f753f8302a953cb6310377bd331ef169d31e3bc
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2026-01-25 17:12:18 -0500

    Time: 197 ms (18.66%) | Memory: 30.8 MB (58.29%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,29 +1,32 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        max_area = 0
-        """for left in range(len(heights)):
-            min_seen = heights[left]
-            for right in range(left,len(heights)):
-                if heights[right] < min_seen:
-                    min_seen = heights[right]
-                area = (right - left + 1)*min_seen
-                if area > max_area:
-                    max_area = area"""
         n = len(heights)
+        if n == 0:
+            return 0
+        left_boundary = [-1]*n
+        right_boundary = [n]*n
         stack = []
-        for idx in range(n):
-            start = idx
-            while stack and stack[-1][0] > heights[idx]:
-                height, left = stack.pop()
-                area = (idx - left)*height 
-                max_area = max(area, max_area)
-                start = left
-            stack.append((heights[idx], start))
-                
-            
-        while stack:
-            height, left = stack.pop()
-            area = (n - left)*height 
-            max_area = max(area, max_area)
 
+        for i in range(n):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+    
+            if stack:
+                left_boundary[i] = stack[-1]
+
+            stack.append(i)
+        
+        stack.clear()
+        for i in range(n-1,-1,-1):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+
+            if stack:
+                right_boundary[i] = stack[-1]
+            stack.append(i)
+
+        max_area = 0
+        for i in range(n):
+            area = heights[i] * (right_boundary[i]-left_boundary[i] - 1)
+            max_area = max(area, max_area)
         return max_area
